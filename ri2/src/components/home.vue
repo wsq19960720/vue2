7643fd44531cd38de48abfb759a6b125054a30d0
wsq <649704398@qq.com>
2019-07-08 08:48:14 +0800

    ri2

---
+++ ri2/src/components/home.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      首页内容
+      <ul>
+          <li>
+              列表一
+          </li>
+           <li>
+              列表二
+          </li>
+      </ul>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
